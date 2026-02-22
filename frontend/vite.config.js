dd2efb75cd29c34c976fc8f12d42419c5c5c16b7
ArjuSuresh <ArjuSuresh@users.noreply.github.com>
2026-02-23 01:29:24 +0530

    Remove SSL plugin for production deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), basicSsl()],
-    server: {
-        host: true, // Listen on all local IPs to test AR on mobile
-        port: 3000
-    }
+    plugins: [react()],
 })
